f9b48d00c19dc74f990407b26efb3115df72c88f
L33N0 <adamleen04@gmail.com>
2025-12-10 18:48:15 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace JokeConsoleApp
+{
+    public struct Joke
+    {
+        public string setup { get; set; }
+        public string punchline { get; set; }
+    }
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+            Console.WriteLine("---Jokes App---");
+
+            while (true)
+            {
+                Console.WriteLine("Wanna hear a joke?(Y/N)");
+
+                ConsoleKeyInfo response = Console.ReadKey(true);
+
+                if (response.Key == ConsoleKey.Y)
+                {
+                    var joke = await client.GetFromJsonAsync<Joke>("https://official-joke-api.appspot.com/random_joke");
+
+                    Console.WriteLine($"\nSetup: {joke.setup}");
+
+                    Console.WriteLine($"Punchline: {joke.punchline}\n");
+                }
+                else if (response.Key == ConsoleKey.N)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nInvalid Option\n");
+                }
+
+            }
+
+            Console.WriteLine("\nExiting...");
+        }
+    }
+}
